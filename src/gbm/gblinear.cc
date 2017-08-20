df255509edd311384d288bdbbd06816ee20523f1
Vadim Khotilovich <khotilovich@gmail.com>
2017-08-20 13:22:21 -0500

    [core] stagewise-like algorithm for gblinear

--- src/gbm/gblinear.cc
+++ src/gbm/gblinear.cc
@@ -7,6 +7,7 @@
  */
 #include <dmlc/omp.h>
 #include <dmlc/parameter.h>
+#include <dmlc/timer.h>
 #include <xgboost/gbm.h>
 #include <xgboost/logging.h>
 #include <vector>
@@ -14,11 +15,18 @@
 #include <sstream>
 #include <cstring>
 #include <algorithm>
+#include <numeric>
 
 namespace xgboost {
 namespace gbm {
 
 DMLC_REGISTRY_FILE_TAG(gblinear);
+
+// algorithm for weight updates
+enum GBLinearMethod {
+  kHogwild,
+  kStagewise
+};
 
 // model parameter
 struct GBLinearModelParam :public dmlc::Parameter<GBLinearModelParam> {
@@ -50,6 +58,14 @@ struct GBLinearTrainParam : public dmlc::Parameter<GBLinearTrainParam> {
   float reg_alpha;
   /*! \brief regularization weight for L2 norm in bias */
   float reg_lambda_bias;
+  /*! \brief flag for whether the loss sum is scaled by 1/N */
+  bool scaled_loss;
+  /*! \brief choice of algorithm for weight updates */
+  int gblinear_method;
+  /*! \brief number of top features to update per boosting round in stagewise algorithm */
+  unsigned top_n;
+  // flag to print out detailed breakdown of runtime
+  int debug_verbose;
   // declare parameters
   DMLC_DECLARE_PARAMETER(GBLinearTrainParam) {
     DMLC_DECLARE_FIELD(learning_rate).set_lower_bound(0.0f).set_default(1.0f)
@@ -60,6 +76,30 @@ struct GBLinearTrainParam : public dmlc::Parameter<GBLinearTrainParam> {
         .describe("L1 regularization on weights.");
     DMLC_DECLARE_FIELD(reg_lambda_bias).set_lower_bound(0.0f).set_default(0.0f)
         .describe("L2 regularization on bias.");
+    DMLC_DECLARE_FIELD(scaled_loss)
+        .set_default(false)
+        .describe("Flag for whether the loss sum term is scaled by 1/N, where N is # of samples. "
+                  "Scaled loss is used, e.g., in sckit-learn and glmnet. Note that the equivalent "
+                  "regularization parameters' magnitudes for non-scaled loss are N times larger.");
+    DMLC_DECLARE_FIELD(gblinear_method)
+        .set_default(kHogwild)
+        .add_enum("hogwild", kHogwild)
+        .add_enum("stagewise", kStagewise)
+        .describe("Algorithm for weight updates during a boosting iteration:\n"
+                  "  hogwild: lock-free cyclic gradient descent (CGD), updating all features\n"
+                  "           in the order approximately guided by the order of columns;\n"
+                  "  stagewise: stagewise CGD, selecting and updating top-N features\n."
+                  "           in the order of decreasing magnitude of univariate weight changes.");
+    DMLC_DECLARE_FIELD(top_n)
+        .set_lower_bound(0u)
+        .set_default(1u)
+        .describe("The number of top features to select and update per boosting round "
+                  "when using stagewise algorithm. Zero means using all the features ordered "
+                  "by decreasing magnitude of their univariate weight changes.");
+    DMLC_DECLARE_FIELD(debug_verbose)
+        .set_lower_bound(0)
+        .set_default(0)
+        .describe("Flag to print out detailed breakdown of runtime.");
     // alias of parameters
     DMLC_DECLARE_ALIAS(learning_rate, eta);
     DMLC_DECLARE_ALIAS(reg_lambda, lambda);
@@ -90,83 +130,51 @@ class GBLinear : public GradientBooster {
   explicit GBLinear(bst_float base_margin)
       : base_margin_(base_margin) {
   }
+
   void Configure(const std::vector<std::pair<std::string, std::string> >& cfg) override {
     if (model.weight.size() == 0) {
       model.param.InitAllowUnknown(cfg);
     }
     param.InitAllowUnknown(cfg);
+    //std::cout<<"PPP:"<<std::endl;
+    //for(auto &a : param.__DICT__()) std::cout<<a.first<<":"<<a.second<<std::endl;
   }
+
   void Load(dmlc::Stream* fi) override {
     model.Load(fi);
   }
+
   void Save(dmlc::Stream* fo) const override {
     model.Save(fo);
   }
+
   void DoBoost(DMatrix *p_fmat,
                std::vector<bst_gpair> *in_gpair,
                ObjFunction* obj) override {
     // lazily initialize the model when not ready.
     if (model.weight.size() == 0) {
       model.InitModel();
-    }
 
-    std::vector<bst_gpair> &gpair = *in_gpair;
-    const int ngroup = model.param.num_output_group;
-    const RowSet &rowset = p_fmat->buffered_rowset();
-    // for all the output group
-    for (int gid = 0; gid < ngroup; ++gid) {
-      double sum_grad = 0.0, sum_hess = 0.0;
-      const bst_omp_uint ndata = static_cast<bst_omp_uint>(rowset.size());
-      #pragma omp parallel for schedule(static) reduction(+: sum_grad, sum_hess)
-      for (bst_omp_uint i = 0; i < ndata; ++i) {
-        bst_gpair &p = gpair[rowset[i] * ngroup + gid];
-        if (p.hess >= 0.0f) {
-          sum_grad += p.grad; sum_hess += p.hess;
-        }
-      }
-      // remove bias effect
-      bst_float dw = static_cast<bst_float>(
-        param.learning_rate * param.CalcDeltaBias(sum_grad, sum_hess, model.bias()[gid]));
-      model.bias()[gid] += dw;
-      // update grad value
-      #pragma omp parallel for schedule(static)
-      for (bst_omp_uint i = 0; i < ndata; ++i) {
-        bst_gpair &p = gpair[rowset[i] * ngroup + gid];
-        if (p.hess >= 0.0f) {
-          p.grad += p.hess * dw;
-        }
+      if (param.gblinear_method == kStagewise) {
+        // pre-allocate the memory
+        deltaw.reserve(model.param.num_feature * model.param.num_output_group);
+        sorted_idx.reserve(model.param.num_feature);
+        top_features.reserve(model.param.num_feature);
       }
     }
-    dmlc::DataIter<ColBatch> *iter = p_fmat->ColIterator();
-    while (iter->Next()) {
-      // number of features
-      const ColBatch &batch = iter->Value();
-      const bst_omp_uint nfeat = static_cast<bst_omp_uint>(batch.size);
-      #pragma omp parallel for schedule(static)
-      for (bst_omp_uint i = 0; i < nfeat; ++i) {
-        const bst_uint fid = batch.col_index[i];
-        ColBatch::Inst col = batch[i];
-        for (int gid = 0; gid < ngroup; ++gid) {
-          double sum_grad = 0.0, sum_hess = 0.0;
-          for (bst_uint j = 0; j < col.length; ++j) {
-            const bst_float v = col[j].fvalue;
-            bst_gpair &p = gpair[col[j].index * ngroup + gid];
-            if (p.hess < 0.0f) continue;
-            sum_grad += p.grad * v;
-            sum_hess += p.hess * v * v;
-          }
-          bst_float &w = model[fid][gid];
-          bst_float dw = static_cast<bst_float>(param.learning_rate *
-                                                param.CalcDelta(sum_grad, sum_hess, w));
-          w += dw;
-          // update grad value
-          for (bst_uint j = 0; j < col.length; ++j) {
-            bst_gpair &p = gpair[col[j].index * ngroup + gid];
-            if (p.hess < 0.0f) continue;
-            p.grad += p.hess * col[j].fvalue * dw;
-          }
-        }
+    double tstart = dmlc::GetTime();
+    switch (param.gblinear_method) {
+      case kHogwild: {
+        UpdateHogwild(p_fmat, in_gpair);
+        break;
       }
+      case kStagewise: {
+        UpdateStagewise(p_fmat, in_gpair);
+        break;
+      }
+    }
+    if (param.debug_verbose > 0) {
+      LOG(INFO) << "DoBoost(): " << 1000 * (dmlc::GetTime() - tstart) << " msec";
     }
   }
 
@@ -177,7 +185,7 @@ class GBLinear : public GradientBooster {
       model.InitModel();
     }
     CHECK_EQ(ntree_limit, 0U)
-        << "GBLinear::Predict ntrees is only valid for gbtree predictor";
+        << "GBLinear::Predict ntrees>0 is not valid for the gblinear booster";
     std::vector<bst_float> &preds = *out_preds;
     const std::vector<bst_float>& base_margin = p_fmat->info().base_margin;
     preds.resize(0);
@@ -204,6 +212,7 @@ class GBLinear : public GradientBooster {
       }
     }
   }
+
   // add base margin
   void PredictInstance(const SparseBatch::Inst &inst,
                std::vector<bst_float> *out_preds,
@@ -304,6 +313,184 @@ class GBLinear : public GradientBooster {
   }
 
  protected:
+
+  void UpdateBias(DMatrix *p_fmat,
+                  std::vector<bst_gpair> *in_gpair) {
+    std::vector<bst_gpair> &gpair = *in_gpair;
+    const int ngroup = model.param.num_output_group;
+    const RowSet &rowset = p_fmat->buffered_rowset();
+    // for all the output group
+    for (int gid = 0; gid < ngroup; ++gid) {
+      double sum_grad = 0.0, sum_hess = 0.0;
+      const bst_omp_uint ndata = static_cast<bst_omp_uint>(rowset.size());
+      #pragma omp parallel for schedule(static) reduction(+: sum_grad, sum_hess)
+      for (bst_omp_uint i = 0; i < ndata; ++i) {
+        bst_gpair &p = gpair[rowset[i] * ngroup + gid];
+        if (p.hess >= 0.0f) {
+          sum_grad += p.grad; sum_hess += p.hess;
+        }
+      }
+      if (param.scaled_loss && ndata > 0) {
+        sum_grad /= ndata; sum_hess /= ndata;
+      }
+      // remove bias effect
+      bst_float dw = static_cast<bst_float>(
+        param.learning_rate * param.CalcDeltaBias(sum_grad, sum_hess, model.bias()[gid]));
+      model.bias()[gid] += dw;
+      // update grad value
+      #pragma omp parallel for schedule(static)
+      for (bst_omp_uint i = 0; i < ndata; ++i) {
+        bst_gpair &p = gpair[rowset[i] * ngroup + gid];
+        if (p.hess >= 0.0f) {
+          p.grad += p.hess * dw;
+        }
+      }
+    }
+  }
+
+  void UpdateHogwild(DMatrix *p_fmat,
+               std::vector<bst_gpair> *in_gpair) {
+    this->UpdateBias(p_fmat, in_gpair);
+    std::vector<bst_gpair> &gpair = *in_gpair;
+    const int ngroup = model.param.num_output_group;
+    dmlc::DataIter<ColBatch> *iter = p_fmat->ColIterator();
+    while (iter->Next()) {
+      const ColBatch &batch = iter->Value();
+      // number of features
+      const bst_omp_uint nfeat = static_cast<bst_omp_uint>(batch.size);
+      // lock-free parallel updates of all features
+      #pragma omp parallel for schedule(static)
+      for (bst_omp_uint i = 0; i < nfeat; ++i) {
+        const bst_uint fid = batch.col_index[i];
+        ColBatch::Inst col = batch[i];
+        for (int gid = 0; gid < ngroup; ++gid) {
+          const bst_uint ndata = col.length;
+          double sum_grad = 0.0, sum_hess = 0.0;
+          for (bst_uint j = 0; j < ndata; ++j) {
+            const bst_float v = col[j].fvalue;
+            bst_gpair &p = gpair[col[j].index * ngroup + gid];
+            if (p.hess < 0.0f) continue;
+            sum_grad += p.grad * v;
+            sum_hess += p.hess * v * v;
+          }
+          if (param.scaled_loss && ndata > 0) {
+            sum_grad /= ndata; sum_hess /= ndata;
+          }
+          bst_float &w = model[fid][gid];
+          bst_float dw = static_cast<bst_float>(param.learning_rate *
+                                                param.CalcDelta(sum_grad, sum_hess, w));
+          w += dw;
+          // update grad values
+          for (bst_uint j = 0; j < col.length; ++j) {
+            bst_gpair &p = gpair[col[j].index * ngroup + gid];
+            if (p.hess < 0.0f) continue;
+            p.grad += p.hess * col[j].fvalue * dw;
+          }
+        }
+      }
+    }
+  }
+
+  void UpdateStagewise(DMatrix *p_fmat,
+                           std::vector<bst_gpair> *in_gpair) {
+    this->UpdateBias(p_fmat, in_gpair);
+    std::vector<bst_gpair> &gpair = *in_gpair;
+    const int ngroup = model.param.num_output_group;
+    const unsigned nfeature = model.param.num_feature;
+
+    // Calculate univariate weight changes
+    deltaw.resize(nfeature * ngroup);
+    std::fill(deltaw.begin(), deltaw.end(), 0.f);
+    dmlc::DataIter<ColBatch> *iter = p_fmat->ColIterator();
+    while (iter->Next()) {
+      const ColBatch &batch = iter->Value();
+      const bst_omp_uint nfeat = static_cast<bst_omp_uint>(batch.size);
+      #pragma omp parallel for schedule(static)
+      for (bst_omp_uint i = 0; i < nfeat; ++i) {
+        const bst_uint fid = batch.col_index[i];
+        ColBatch::Inst col = batch[i];
+        for (int gid = 0; gid < ngroup; ++gid) {
+          const bst_uint ndata = col.length;
+          double sum_grad = 0.0, sum_hess = 0.0;
+          for (bst_uint j = 0; j < ndata; ++j) {
+            const bst_float v = col[j].fvalue;
+            bst_gpair &p = gpair[col[j].index * ngroup + gid];
+            if (p.hess < 0.0f) continue;
+            sum_grad += p.grad * v;
+            sum_hess += p.hess * v * v;
+          }
+          if (param.scaled_loss && ndata > 0) {
+            sum_grad /= ndata; sum_hess /= ndata;
+          }
+          bst_float dw = static_cast<bst_float>(param.learning_rate *
+                           param.CalcDelta(sum_grad, sum_hess, model[fid][gid]));
+          if (dw != 0) deltaw[gid * nfeature + fid] = dw;
+        }
+      }
+    }
+
+    // Update top-N features that have the largest univariate abs weight changes
+    const unsigned max_n = param.top_n > 0 ? param.top_n : nfeature;
+    bst_float *pdeltaw = &deltaw[0];
+    for (int gid = 0; gid < ngroup; ++gid) {
+      const size_t g_offset = gid * nfeature;
+      // get the indices of sorted deltaw (in descending order of abs values)
+      sorted_idx.resize(nfeature);
+      std::iota(sorted_idx.begin(), sorted_idx.end(), g_offset);
+      std::sort(sorted_idx.begin(), sorted_idx.end(),
+                [pdeltaw](size_t i, size_t j) {
+                  return std::abs(*(pdeltaw + i)) > std::abs(*(pdeltaw + j));
+                });
+      bst_float max_dw = deltaw[sorted_idx[0]];
+      if (max_dw == 0.f) continue;
+      // Pick top-N features, and do cyclic updates only for them.
+      // NOTE: a possibility of several features having the same weight is ignored.
+      top_features.clear();
+      for (auto &i: sorted_idx) {
+        if (deltaw[i] == 0.f || top_features.size() == max_n) break;
+        top_features.push_back(i - g_offset);
+      }
+      iter = p_fmat->ColIterator(top_features);
+      while (iter->Next()) {
+        const ColBatch &batch = iter->Value();
+        const bst_uint nfeat = static_cast<bst_omp_uint>(batch.size);
+        for (bst_uint i = 0; i < nfeat; ++i) {
+          const bst_uint fid = batch.col_index[i];
+          ColBatch::Inst col = batch[i];
+          const bst_omp_uint ndata = static_cast<bst_omp_uint>(col.length);
+          bst_float &w = model[fid][gid];
+          bst_float dw;
+          if (i == 0) {
+            dw = max_dw; // optimization for the 1st feature: no need to update dw
+          } else {
+            double sum_grad = 0.0, sum_hess = 0.0;
+            #pragma omp parallel for schedule(static) reduction(+: sum_grad, sum_hess)
+            for (bst_omp_uint j = 0; j < ndata; ++j) {
+              const bst_float v = col[j].fvalue;
+              bst_gpair &p = gpair[col[j].index * ngroup + gid];
+              if (p.hess < 0.0f) continue;
+              sum_grad += p.grad * v;
+              sum_hess += p.hess * v * v;
+            }
+            if (param.scaled_loss && ndata > 0) {
+              sum_grad /= ndata; sum_hess /= ndata;
+            }
+            dw = static_cast<bst_float>(param.learning_rate *
+                                        param.CalcDelta(sum_grad, sum_hess, w));
+          }
+          w += dw;
+          // update grad values
+          #pragma omp parallel for schedule(static)
+          for (bst_omp_uint j = 0; j < ndata; ++j) {
+            bst_gpair &p = gpair[col[j].index * ngroup + gid];
+            if (p.hess < 0.0f) continue;
+            p.grad += p.hess * col[j].fvalue * dw;
+          }
+        }
+      }
+    }
+  }
+
   inline void Pred(const RowBatch::Inst &inst, bst_float *preds, int gid, bst_float base) {
     bst_float psum = model.bias()[gid] + base;
     for (bst_uint i = 0; i < inst.length; ++i) {
@@ -312,6 +499,7 @@ class GBLinear : public GradientBooster {
     }
     preds[gid] = psum;
   }
+
   // model for linear booster
   class Model {
    public:
@@ -356,8 +544,12 @@ class GBLinear : public GradientBooster {
   Model model;
   // training parameter
   GBLinearTrainParam param;
-  // Per feature: shuffle index of each feature index
-  std::vector<bst_uint> feat_index;
+
+  // The following three are used by the stagewise algorithm.
+  // Having them as class members helps to avoid repeated construction & memory allocs.
+  std::vector<bst_float> deltaw;
+  std::vector<size_t> sorted_idx;
+  std::vector<bst_uint> top_features;
 };
 
 // register the objective functions
